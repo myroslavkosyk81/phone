01c71a039ad594672733720d34fe5a9238dd08ab
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2024-05-15 14:15:14 +0300

    top and left bars complete

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -1,25 +1,35 @@
-
+"use client"
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 import { navLinks } from '@/lib/constants'
+import { usePathname } from 'next/navigation'
 
 const LeftSideBar = () => {
+   const pathName = usePathname();
   return (
-    <div className=' h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>
-      <Image src="/logo.png" alt='logo' width={70} height={40} />
-      <div className='flex flex-col gap-12'>
-         {navLinks.map((link) => (
-            <Link href={link.url} key={link.label} className=' flex gap-4 text-body-medium'>{link.icon} <p>{link.label}</p></Link>
-         ))}
+    <div className=" h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden">
+      <Image src="/logo.png" alt="logo" width={70} height={40} />
+      <div className="flex flex-col gap-12">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className={`flex gap-4 text-body-medium ${
+              pathName === link.url ? "text-blue-1" : "text-grey-1"
+            }`}
+          >
+            {link.icon} <p>{link.label}</p>
+          </Link>
+        ))}
       </div>
-      <div className=' flex gap-4 text-body-medium items-center'>
-         <UserButton />
-         <p>Edit Profile</p>
+      <div className=" flex gap-4 text-body-medium items-center">
+        <UserButton />
+        <p>Edit Profile</p>
       </div>
     </div>
-  )
+  );
 }
 export default LeftSideBar
--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,27 +1,39 @@
+"use client"
 import { navLinks } from '@/lib/constants'
 import { UserButton } from '@clerk/nextjs'
 import { Menu } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useState } from 'react'
 
 const TopBar = () => {
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+  const pathName = usePathname();
+
   return (
-    <div className=" sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 b-blue-2 shadow-xl lg:hidden">
+    <div className=" sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 bg-blue-2 shadow-xl lg:hidden">
       <Image src="/logo.png" alt="logo" width={70} height={40} />
       <div className="flex gap-8 max-md:hidden">
         {navLinks.map((link) => (
           <Link
             href={link.url}
             key={link.label}
-            className=" flex gap-4 text-body-medium"
+            className={`flex gap-4 text-body-medium ${pathName === link.url ? "text-blue-1" : "text-grey-1"}`}
           >
             <p>{link.label}</p>
           </Link>
         ))}
       </div>
-      <div className=" flex gap-4 items-center">
-        <Menu className=' cursor-pointer md:hidden' />
+      <div className=" relative flex gap-4 items-center">
+        <Menu className=' cursor-pointer md:hidden' onClick={() => setDropdownMenu(!dropdownMenu)} />
+        {dropdownMenu && (
+          <div className=' absolute  top-10 right-6 flex flex-col gap-8 p-5 bg-white shadow-xl rounded-lg'>
+          {navLinks.map((link) => (
+             <Link href={link.url} key={link.label} className=' flex gap-4 text-body-medium'>{link.icon} <p>{link.label}</p></Link>
+          ))}
+       </div>
+        )}
         <UserButton />
         
       </div>
